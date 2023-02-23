da168bd6e577ad9b787d3270f2a5e32b3c2ecc70
splazt <120243136+splazt@users.noreply.github.com>
2023-02-24 02:48:11 +0900

    basic redux store->reducer->action->useDispatch->useSelector implemented

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import './style.css';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { increase, decrease } from './index.js;
+import { useSelector, useDispatch } from 'react-redux';
+import { increase, decrease } from './index.js';
 
 export default function App() {
-  const plusNum = () => {};
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
 
-  const minusNum = () => {};
+  const plusNum = () => {
+    dispatch(increase());
+  };
+
+  const minusNum = () => {
+    dispatch(decrease());
+  };
 
   return (
     <div className="container">
-      <h1>{`Count: ${1}`}</h1>
+      <h1>{`Count: ${state}`}</h1>
       <div>
         <button className="plusBtn" onClick={plusNum}>
           +
--- src/index.js
+++ src/index.js
@@ -7,4 +7,32 @@ import App from './App';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
-root.render(<App />);
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREASE':
+      return state + 1;
+    case 'DECREASE':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+export const increase = () => {
+  return {
+    type: 'INCREASE',
+  };
+};
+
+export const decrease = () => {
+  return {
+    type: 'DECREASE',
+  };
+};
+const store = createStore(reducer);
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
